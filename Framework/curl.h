f41ed19a3bf67373ea1b0e8303c57721232e7c98
Ian McDowell <me@ianmcdowell.net>
2017-12-28 23:12:26 -0800

    Initial commit

---
+++ Framework/curl.h
@@ -0,0 +1,19 @@
+//
+//  curl.h
+//  curl
+//
+//  Created by Ian McDowell on 12/28/17.
+//  Copyright © 2017 Ian McDowell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for curl.
+FOUNDATION_EXPORT double curlVersionNumber;
+
+//! Project version string for curl.
+FOUNDATION_EXPORT const unsigned char curlVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <curl/PublicHeader.h>
+
+
